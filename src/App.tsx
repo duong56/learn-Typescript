b01649d04268681004da5b938ce5def3373f7620
duong56 <91894323+duong56@users.noreply.github.com>
2022-10-27 19:15:54 +0700

    Finish chapter4 Funtion

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+
+  let sum = add(10, 20);
+
+  console.log(sum);
+
+  function multiply(a: number, b: number, c?: number): number {
+    if (typeof c !== 'undefined') {
+      return a * b * c;
+    }
+
+    return a * b;
+  }
+
+  console.log(multiply(1, 2, 3))
+
+  function multiply(a: number, b?: number, c: number): number {
+    if (typeof c !== 'undefined') {
+      return a * b * c;
+    }
+    return a * b;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
